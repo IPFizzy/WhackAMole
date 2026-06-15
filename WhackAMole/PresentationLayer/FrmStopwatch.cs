7b924676609bc351267ede5f045e053863c27582
IPFizzy <fizzy@keonbushman.com>
2026-06-15 15:03:43 -0700

    Add functionality to move the target as well as hide it when clicked by the user.

--- WhackAMole/PresentationLayer/FrmStopwatch.cs
+++ WhackAMole/PresentationLayer/FrmStopwatch.cs
@@ -15,6 +15,8 @@ namespace WhackAMole
     {
         // Class level variable to hold the timers time
         TimeSpan timeElapsed = new TimeSpan();
+        // Create a new Random object to generate numbers
+        Random random = new Random();
 
         public FrmStopwatch()
         {
@@ -64,6 +66,7 @@ namespace WhackAMole
         /// <summary>
         /// Tick Event Handler for tmrStopwatch
         /// Updates the timeElapsed variable and the label
+        /// Moves btnTarget every three seconds
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
@@ -75,6 +78,31 @@ namespace WhackAMole
             timeElapsed = timeElapsed.Add(TimeSpan.FromMilliseconds(interval));
             // Show the timeElapsed on the label
             lblTimeElapsed.Text = timeElapsed.ToString();
+            // Check if it is time to move the target button
+            if (timeElapsed.Seconds % 3 == 0)
+            {
+                // Select a new location for the top of btnTarget
+                // Randomly generate a location for the top of the button
+                // between 0 and the form height minus the button height
+                btnTarget.Top = random.Next(0, this.Height - btnTarget.Height);
+                // Select a new location for the left side of btnTarget
+                btnTarget.Left = random.Next(0, this.Height - btnTarget.Width);
+                // Get random numbers for the RGB color for the button
+                btnTarget.BackColor = Color.FromArgb(random.Next(0, 256), random.Next(0, 256), random.Next(0, 256));
+                // Set the target to be visible
+                btnTarget.Visible = true;
+            }
+        } // End of TmrStopwatchTickEH
+
+        /// <summary>
+        /// Click Event Handler for btnTarget tp hide the target
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnTargetClickEH(object sender, EventArgs e)
+        {
+            // Hide the target
+            btnTarget.Visible = false;
         }
     }
 }
