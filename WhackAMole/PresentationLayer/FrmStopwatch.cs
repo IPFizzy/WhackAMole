e5acb33f622489bd5a6d347a62b201dea244947e
IPFizzy <fizzy@keonbushman.com>
2026-06-15 13:45:11 -0700

    Finish the UI and add stop and start functionality to the timer.

---
+++ WhackAMole/PresentationLayer/FrmStopwatch.cs
@@ -0,0 +1,43 @@
+/*
+ * Keon Bushman
+ * CST - 250
+ * 06/21/2026
+ * Whack-A-Mole
+ * Activity 5
+ * Activity 5 Guide
+ */
+
+namespace WhackAMole
+{
+    public partial class FrmStopwatch : Form
+    {
+        public FrmStopwatch()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Click Event Handler for btnStart
+        /// Starts the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStartClickEH(object sender, EventArgs e)
+        {
+            // Start timer
+            tmrStopwatch.Start();
+        }
+
+        /// <summary>
+        /// Click Event Handler for btnStop
+        /// Stops the timer
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void BtnStopClickEH(object sender, EventArgs e)
+        {
+            // Stop timer
+            tmrStopwatch.Stop();
+        }
+    }
+}
